6ee501c56bbda851538b7319d501cf7ee85e4136
glbarreto <55640976+glbarreto@users.noreply.github.com>
2023-06-20 09:27:01 -0300

    v.3 server.py

--- server.py
+++ server.py
@@ -3,85 +3,112 @@ import os
 import threading
 import datetime
 
-HOST = '172.20.192.1'
-PORT = 800
-DIRECTORY = 'scratch/convidado/Downloads/Meu-Servidor-main/Descubra/'
+HOST = 'localhost'
+PORT = 8000
+DIRECTORY = '/path/to/directory'  # Diretório que será servido
+AUDIO_PATH = '/path/to/audio.mp3'  # Caminho do arquivo de áudio
 
 def handle_request(client_socket, client_address):
     # Recebe a requisição do cliente
-    requisicoes = client_socket.recv(1024).decode('utf-8')
+    request = client_socket.recv(1024).decode('utf-8')
     print(f'Received request from {client_address[0]}:{client_address[1]}')
 
-    request_parts = requisicoes.split(' ')
-    metodo = request_parts[0]
-    caminho = request_parts[1]
+    # Analisa a requisição para determinar o caminho solicitado
+    request_parts = request.split(' ')
+    method = request_parts[0]
+    path = request_parts[1]
 
-    if metodo == 'GET':
-        if caminho == '/':
-            resposta = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
-            resposta += '<!DOCTYPE html>\n<html>\n<head>\n<title>Meu Servidor</title>\n<style>\n'
-            arquivos = os.listdir(DIRECTORY)
-            resposta = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
-            resposta += '<h1>Arquivos no diretorio:</h1>\n'
-            for file in arquivos:
+    if method == 'GET':
+        if path == '/':
+            # Retorna a lista de arquivos do diretório com links clicáveis para os diretórios
+            files = os.listdir(DIRECTORY)
+            response = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
+            response += '<h1>Arquivos no diretório:</h1>\n'
+            for file in files:
                 file_path = os.path.join(DIRECTORY, file)
                 if os.path.isdir(file_path):
                     file_link = f'<a href="{file}/">{file}/</a><br>'
                 else:
                     file_link = f'<a href="{file}">{file}</a><br>'
-                resposta += file_link
-            resposta += '</style>\n<script>\n'
-            resposta += 'window.addEventListener("DOMContentLoaded", function() {\n'
-            resposta += '  var audio = document.getElementById("myAudio");\n'
-            resposta += '  audio.play();\n'
-            resposta += '});\n'
-            resposta += '</script>\n</head>\n<body>\n<h1>You got...</h1>\n'
-            resposta += '<audio id="myAudio" controls autoplay>\n'
-            resposta += '<source src="audio.mp3" type="audio/mpeg">\n'
-            resposta += '</audio>\n</body>\n</html>'
-        elif caminho == '/HEADER':
-            #Cabeçalho HTTP da requisição
-            resposta = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'
-            resposta += requisicoes
-        elif caminho == '/info':
-            resposta = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
-            resposta += '<h1>Informacoes sobre o servidor e o computador:</h1>\n'
-            resposta += f'Data: {datetime.datetime.now()}<br>'
-            resposta += f'Usuário: {os.getlogin()}<br>'
-            resposta += f'SO: {os.name}<br>'
-        elif caminho == '/hello':
-            resposta = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'
-            resposta += 'hello\n'
-        else:
-            try:
-                with open(DIRECTORY + caminho, 'rb') as file:
-                    file_data = file.read()
-                resposta = 'HTTP/1.1 200 OK\nContent-Type: application/octet-stream\n\n'
-                response_binary = bytes(resposta, 'utf-8') + file_data
-                client_socket.sendall(response_binary)
-                client_socket.close()
-                return
-            except FileNotFoundError:
-                resposta = 'HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\n'
-                resposta += '404 Not Found\n'
+                response += file_link
 
-    response_bytes = bytes(resposta, 'utf-8')
+            # Adiciona a reprodução automática do áudio
+            response += '<h1>You got...</h1>\n'
+            response += f'<audio controls autoplay><source src="/audio" type="audio/mpeg"></audio>'
+        elif path == '/HEADER':
+            # Retorna o cabeçalho HTTP da requisição
+            response = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'
+            response += request
+        elif path == '/info':
+            # Retorna informações sobre o servidor e o computador
+            response = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'
+            response += f'Data: {datetime.datetime.now()}\n'
+            response += f'Usuário: {os.getlogin()}\n'
+            response += f'SO: {os.name}\n'
+        elif path == '/hello':
+            # Responde com "hello"
+            response = 'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n'
+            response += 'hello\n'
+        elif path == '/audio':
+            # Retorna o arquivo de áudio
+            with open(AUDIO_PATH, 'rb') as file:
+                audio_data = file.read()
+            response = 'HTTP/1.1 200 OK\nContent-Type: audio/mpeg\n\n'
+            response_binary = bytes(response, 'utf-8') + audio_data
+            client_socket.sendall(response_binary)
+            client_socket.close()
+            return
+        else:
+            # Tenta ler o arquivo ou abrir o diretório solicitado
+            file_path = os.path.join(DIRECTORY, path.lstrip('/'))
+            if os.path.isfile(file_path):
+                with open(file_path, 'rb') as file:
+                    file_data = file.read()
+                response = 'HTTP/1.1 200 OK\nContent-Type: application/octet-stream\n\n'
+                response_binary = bytes(response, 'utf-8') + file_data
+            elif os.path.isdir(file_path):
+                # Retorna a lista de arquivos e diretórios dentro do diretório solicitado
+                files = os.listdir(file_path)
+                response = 'HTTP/1.1 200 OK\nContent-Type: text/html\n\n'
+                response += f'<h1>Arquivos no diretório {path}:</h1>\n'
+                for file in files:
+                    file_path = os.path.join(file_path, file)
+                    if os.path.isdir(file_path):
+                        file_link = f'<a href="{os.path.join(path, file)}/">{file}/</a><br>'
+                    else:
+                        file_link = f'<a href="{os.path.join(path, file)}">{file}</a><br>'
+                    response += file_link
+            else:
+                response = 'HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\n'
+                response += '404 Not Found\n'
+    else:
+        response = 'HTTP/1.1 501 Not Implemented\nContent-Type: text/plain\n\n'
+        response += '501 Not Implemented\n'
+
+    # Envia a resposta ao cliente
+    response_bytes = bytes(response, 'utf-8')
     client_socket.sendall(response_bytes)
     client_socket.close()
 
-def rodar_server():
+
+# Função principal para iniciar o servidor
+def run_server():
+    # Cria o socket do servidor
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen(1)
     print(f'Server listening on {HOST}:{PORT}')
 
+    # Loop principal para aceitar conexões dos clientes
     while True:
         # Aguarda uma conexão
         client_socket, client_address = server_socket.accept()
+
+        # Cria uma thread para tratar a requisição do cliente
         client_thread = threading.Thread(target=handle_request, args=(client_socket, client_address))
         client_thread.start()
 
 
 if __name__ == '__main__':
-    rodar_server()
+    run_server()
